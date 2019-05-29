9439a20ba55baca1e62a9a124e4958c0c60f518c
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-05-29 15:18:17 +0100

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -41,6 +41,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
+  test "login with valid information followed by logout_2" do
+    #fixing 2 subtle bugs --> 9.1.4 Two subtle bugs (login/logout 2 different browsers)
+    # {https://www.railstutorial.org/book/advanced_login#sec-two_subtle_bugs}
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path #<----- isseur DELETE RESTFUL request
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -52,4 +77,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty? #alternatively written below
     #assert_select 'div.alert-success', 0
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
+
 end
